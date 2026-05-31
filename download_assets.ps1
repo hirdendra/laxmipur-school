a802d6a9c7d93c0d049e0f133a007d779b594d39
Abhi <abhi30@users.noreply.github.com>
2026-05-31 14:37:38 +0545

    Initial commit of Laxmipur School Heritage Website

---
+++ download_assets.ps1
@@ -0,0 +1,66 @@
+# Laxmipur School Heritage Website Asset Downloader
+
+$Screens = @{
+    "emblem" = @{
+        Title = "Laxmipur School Emblem"
+        Screenshot = "https://lh3.googleusercontent.com/aida/ADBb0ujCLDGKbM7XBTzmBGzkFvdhIaBXjWU8sw5zpyWqK_0U56pWqTR6LRbB1h-OGchoO8avgXH-lHQT3Ke6r9aUtqBm4XLZuIBusVbsuZO01eNVFLgP8UX2KhQ_qVJPMq1uYcarV2u91LedEoAPwM8IgWIP3i7TS1qMe-M1jSWZ9yXUANhZ4BJ3iUrtIR5lRHrA42SKk4U_7p98Q0fXEFpAvEL5dddnYPhlJ0Lp6GSc0gPph3_0ILh7HrDygVOA"
+        Code = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzRkNDk4M2I3YjFhMDQ3NzU5NTllMDQ4NTkzMjUzNDk1EgsSBxCQtLHW6h8YAZIBJAoKcHJvamVjdF9pZBIWQhQxMDA2NTcxNjg1OTU2MjczNTAyNg&filename=&opi=89354086"
+        Ext = "svg"
+    }
+    "homepage" = @{
+        Title = "Homepage - Laxmipur English Secondary School"
+        Screenshot = "https://lh3.googleusercontent.com/aida/ADBb0uiX876REKAw7YTnP47y2x01lGaMXCSHFIpU-PJezfbpKb46xsDHzzSrjm_GhcoD1Gp0qGtoAJpQZlPPdZxXrIH1luIQ0RdhjSQl5qWozMsLcbAPhsQuGRgyHJXkGknHUfR8x2fU13Wtny_YPo3HP0PHLFS0c-AiHH-1CT5zpZduuL4PUbsQXRt6PLEWwn-Zg9XBrpStEC2QpKMmB3HhL6WrdYfy5vbskvfB-J2yL-wbDsp5Y3p7o_AKLj1g"
+        Code = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzExZjYyYWQzYjY1ZjRkYzJhMzZjYzk1ZjMzNGViZTlhEgsSBxCQtLHW6h8YAZIBJAoKcHJvamVjdF9pZBIWQhQxMDA2NTcxNjg1OTU2MjczNTAyNg&filename=&opi=89354086"
+        Ext = "html"
+    }
+    "academics" = @{
+        Title = "Academics - Learning Journey"
+        Screenshot = "https://lh3.googleusercontent.com/aida/ADBb0uhziJAtW53GyelOjPFGsd6DoaG1DorLMXt4V8mdQbS4GJj7ZLGUuxzFe4Eoe16wP-orcP6BGSwm2N283n6ATmDY-_GPimQAKqOxxo0sHYaSr23MXCM-RvT8xu0ClH8bcCbLArRqEzrxq_XhbRSshoihnbdrnCn0Ovv60NDSUjcbbw4RdkVQxLUkJBKDkdv7yEgY9l2ciIAE5Vk84JbfPQjpyEgc-QOlLEEaEHOp5EBESK0Mvw6_xqODNfUS"
+        Code = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzViNDliYTBkYTBlMjQ4NzVhOWMyZDg2MTI4YjNhY2I0EgsSBxCQtLHW6h8YAZIBJAoKcHJvamVjdF9pZBIWQhQxMDA2NTcxNjg1OTU2MjczNTAyNg&filename=&opi=89354086"
+        Ext = "html"
+    }
+    "admissions" = @{
+        Title = "Admissions - Join Our Community"
+        Screenshot = "https://lh3.googleusercontent.com/aida/ADBb0uicL9nXtWkcvycnwpd2rjhwTh7qaoeeMH721_7rokqBuBY0YpdaO16LNLEOKcpoKeR-UADXHWs-OQgKSlIrMPUqaPuilfhjilAORUNYovkYqeDF2sXH_bO4MMadldW_ReHccYG62NDI0PuL3hMCk1Soa_irOMUylUiyvnIj3Gh6i-HbdyCqedq211VpeS0cXG6QdegiKSWWipzMOX_Dukg10B5S2HqkNQHHQOJfiiYrxxZqBB4Izp3jVoE"
+        Code = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzczOTdiNDJhYzk1ZDQ3NzM4ZTc2MTc2OWY5MWFkYWY3EgsSBxCQtLHW6h8YAZIBJAoKcHJvamVjdF9pZBIWQhQxMDA2NTcxNjg1OTU2MjczNTAyNg&filename=&opi=89354086"
+        Ext = "html"
+    }
+    "about_us" = @{
+        Title = "About Us - Our Heritage & Mission"
+        Screenshot = "https://lh3.googleusercontent.com/aida/ADBb0uib2Oj1Hg7zoT-T0qRu4_2ofMHn06Q2znYQpni1U61dpqeKYcmfRbriJqgzKeJrQRj-n5B479MOJIqUVxsmmYBx-M79yLVgBm3GZIMm3c80k1N5lKiWupf6Xvhrsk0sZR1kbtRRaAGHwcBWD0sT2Suq1jNLUkk9McJGfd25hwhoTshlXvlYWn8DXiwnMip5ebeQutVCLQ86EAwRF5NqZU5El9RxoiqXu4l9sZ1C3PnWKaK7El_F7u5yYbbQ"
+        Code = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzBkMmQxOTE0OWFlMjQ0NTZhN2Y4NjdkN2ZmMzUxOTZkEgsSBxCQtLHW6h8YAZIBJAoKcHJvamVjdF9pZBIWQhQxMDA2NTcxNjg1OTU2MjczNTAyNg&filename=&opi=89354086"
+        Ext = "html"
+    }
+    "student_life" = @{
+        Title = "Student Life & Activities"
+        Screenshot = "https://lh3.googleusercontent.com/aida/ADBb0uhVzJbDSK89fFZISiffE7im9p0ePCcn1RxagRgC2rFth6y5gL7neCUpYBJd7GLMJhsJ0BGf9kTtuCaHl7xtuYBCWl5yc6bkWtaLr9NC75ORR7f-2bU0R25XWROr0GjDjgLVVO7VKLt5MVSmztWDMPNVqUGoaui54FRbbT4clcqqq4F-poDQPXnLVwgtVnczxgIcWpZLo-7KSJjWrsLN5xzcOkd6v2YPC9ARAjOM4krNkBFkecgf2hFmZjE"
+        Code = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzA1OWU4YWFmZWVjYTQ0YmJiNDAxNzZlZjI0MzRhNWJhEgsSBxCQtLHW6h8YAZIBJAoKcHJvamVjdF9pZBIWQhQxMDA2NTcxNjg1OTU2MjczNTAyNg&filename=&opi=89354086"
+        Ext = "html"
+    }
+    "contact_us" = @{
+        Title = "Contact Us - Visit Tokha"
+        Screenshot = "https://lh3.googleusercontent.com/aida/ADBb0uhHRujwVadrNmv8mM1Vhth3O3il3qhh3h17_dPOWt4lN-CdBwkAcTb6u67HoLBQLmLik1uErYVIPvjdCXXN3kr0fD7V85fr6N0hwCvZrAyhCzvMwVyUhJGg21aMxHg-DR2XHh0L7jy7Fr67YY2qX6aBhGUQEf8PuZIf9jsSCQapEbCUaKzFP7CAREkJNcTu4yiaV8nMBC4Qxka5v1_NQL3aLtWERMtyr0nrkSRyD837DCfz-U-aja-2XKYV"
+        Code = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzMwYzZiYmFlNjVjMTQ0NjU4MGVlOGI3N2QwMWNjOWQ3EgsSBxCQtLHW6h8YAZIBJAoKcHJvamVjdF9pZBIWQhQxMDA2NTcxNjg1OTU2MjczNTAyNg&filename=&opi=89354086"
+        Ext = "html"
+    }
+}
+
+$BaseDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+foreach ($Key in $Screens.Keys) {
+    $Info = $Screens[$Key]
+    $Folder = Join-Path $BaseDir $Key
+    if (-not (Test-Path $Folder)) {
+        New-Item -ItemType Directory -Path $Folder | Out-Null
+    }
+    
+    $ScreenshotPath = Join-Path $Folder "screenshot.png"
+    Write-Host "Downloading screenshot for $($Info.Title)..."
+    Invoke-WebRequest -Uri $Info.Screenshot -OutFile $ScreenshotPath -Headers @{"User-Agent"="Mozilla/5.0"}
+    
+    $CodePath = Join-Path $Folder "index.$($Info.Ext)"
+    Write-Host "Downloading code for $($Info.Title)..."
+    Invoke-WebRequest -Uri $Info.Code -OutFile $CodePath -Headers @{"User-Agent"="Mozilla/5.0"}
+}
+
+Write-Host "All downloads complete!" -ForegroundColor Green
